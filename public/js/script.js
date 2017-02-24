2e69f0d90ae0ede29a6e51532d3f9d25711a6336
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2017-02-24 15:55:02 -0500

    individual drop downs working

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,57 @@
-// Hide sub menu until clicked
+// WORKS - Hide sub menu until clicked
 $(".sub-menu").hide(function() {
 })
 
-// Show sub menu 1 when clicked (toggle open and close)
+// WORKS - Show sub menu 1 when clicked (toggle open and close)
 $( "#drop-down1" ).click(function() {
   $( "#sub-menu1" ).toggle("fast", function(){
   })
 });
 
 
-// Show drop down 2 when clicked
-$( ".click-me1" ).click(function() {
-  $( "#drop-down2" ).show("fast", function() {
-  });
-});
 
 
-// Show sub menu 2 when clicked
+
+
+// WORKS - Show drop down 2 when clicked
+var dropDown2 = function() {
+  $("#drop-down-second").show("fast")
+}
+
+$(".click-me1").click(dropDown2)
+
+// $( ".click-me1" ).click(function() {
+//   $( "#drop-down2" ).show("fast", function() {
+//   });
+// });
+
+
+
+
+
+
+
+// WORKS - Show sub menu 2 when clicked
 $( "#drop-down2" ).click(function() {
   $( "#sub-menu2" ).toggle("fast", function(){
   })
 });
 
+// Show sub menu 3
+$( "#drop-down3" ).click(function() {
+  $( "#sub-menu3" ).toggle("fast", function(){
+  })
+});
 
-// $( "#drop-down3" ).click(function() {
-//   $( "#sub-menu3" ).toggle("fast", function(){
-//   })
-// });
 
-// $( "#drop-down4" ).click(function() {
-//   $( "#sub-menu4" ).toggle("fast", function(){
-//   })
-// });
+// HALF WORKS - Button for On-Click to add a new row
+$("#button1").click(function() {
+  $("#row-2").append('#drop-down4')
+})
+
+// Show sub menu 4
+$( "#drop-down4" ).click(function() {
+  $( "#sub-menu4" ).toggle("fast", function(){
+  })
+});
 
