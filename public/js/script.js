5189108ac2892bc0fc2d22d4b4eb4f26440f0297
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2017-02-24 16:53:40 -0500

    drop downs working and hiding after click

--- public/js/script.js
+++ public/js/script.js
@@ -2,56 +2,80 @@
 $(".sub-menu").hide(function() {
 })
 
+
+// var dropDown1 = function() {
+//   $("sub-menu1").hide("fast")
+// }
+
+
 // WORKS - Show sub menu 1 when clicked (toggle open and close)
 $( "#drop-down1" ).click(function() {
   $( "#sub-menu1" ).toggle("fast", function(){
   })
 });
 
-
-
+// WORKS - Hides drop-down once selection's been made
+$(".wrap-menu").click(function(){
+  $("#sub-menu1").hide("slow");
+});
+// WORKS - Hides the menu when clicking anywhere but inside the drop-down
+$("#drop-down1").click(function(e){
+  e.stopPropagation();
+});
 
 
 
 // WORKS - Show drop down 2 when clicked
 var dropDown2 = function() {
-  $("#drop-down-second").show("fast")
+  $("#drop-down-second").show("slow")
 }
-
 $(".click-me1").click(dropDown2)
-
-// $( ".click-me1" ).click(function() {
-//   $( "#drop-down2" ).show("fast", function() {
-//   });
-// });
-
-
-
-
-
-
-
 // WORKS - Show sub menu 2 when clicked
 $( "#drop-down2" ).click(function() {
   $( "#sub-menu2" ).toggle("fast", function(){
   })
 });
 
+// WORKS - Hides drop-down once selection's been made
+$(".wrap-menu").click(function(){
+  $("#sub-menu2").hide("slow");
+});
+// WORKS - Hides the menu when clicking anywhere but inside the drop-down
+$("#drop-down2").click(function(e){
+  e.stopPropagation();
+});
+
+
+
 // Show sub menu 3
+var dropDown3 = function() {
+  $("#drop-down-third").show("slow")
+}
+$(".click-me2").click(dropDown3)
+// WORKS - Show sub menu 3 when clicked
 $( "#drop-down3" ).click(function() {
   $( "#sub-menu3" ).toggle("fast", function(){
   })
 });
 
 
-// HALF WORKS - Button for On-Click to add a new row
-$("#button1").click(function() {
-  $("#row-2").append('#drop-down4')
-})
 
-// Show sub menu 4
+// WORKS - Show sub menu 4
+var newRow = function() {
+  $("#drop-down-fourth").show("fast")
+}
+$("#button1").click(newRow)
+// WORKS - Show sub menu 3 when clicked
 $( "#drop-down4" ).click(function() {
   $( "#sub-menu4" ).toggle("fast", function(){
   })
 });
 
+// WORKS - Hides drop-down once selection's been made
+$(".wrap-menu").click(function(){
+  $("#sub-menu4").hide("slow");
+});
+// WORKS - Hides the menu when clicking anywhere but inside the drop-down
+$("#drop-down4").click(function(e){
+  e.stopPropagation();
+});
