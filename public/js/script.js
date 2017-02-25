a4845248c581f92064fa1e64818dd33dbceadaa0
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2017-02-25 12:03:24 -0500

    working drop downs almost completed

--- public/js/script.js
+++ public/js/script.js
@@ -2,60 +2,72 @@
 $(".sub-menu").hide(function() {
 })
 
-// Fake Menus
+// FAKE DROP DOWN 1
 $( "#fake-drop-down1" ).click(function() {
   $( "#fake-menu1" ).toggle("slow", function(){
-  })
-});
-$( "#fake-drop-down2" ).click(function() {
-  $( "#fake-menu2" ).toggle("slow", function(){
   })
 });
 // Hides drop-down once selection's been made
 $(".wrap-menu").click(function(){
   $("#fake-menu1").hide("slow");
 });
-$(".wrap-menu").click(function(){
-  $("#fake-menu2").hide("slow");
-});
 // Hides the menu when clicking anywhere but inside the drop-down
 $("#fake-drop-down1").click(function(e){
   e.stopPropagation();
 });
+
+
+// FAKE DROP DOWN 2
+var fakeDropDown2 = function() {
+  $("#static-drop-down2").show("slow")
+}
+$("#button1").click(fakeDropDown2)
+// WORKS - Show sub menu 2 when clicked
+$( "#fake-drop-down2" ).click(function() {
+  $( "#fake-menu2" ).toggle("slow", function(){
+  })
+});
+// WORKS - Hides drop-down once selection's been made
+$(".wrap-menu").click(function(){
+  $("#fake-menu2").hide("slow");
+});
+// WORKS - Hides the menu when clicking anywhere but inside the drop-down
 $("#fake-drop-down2").click(function(e){
   e.stopPropagation();
 });
 
 
-
-// WORKS - Show sub menu 1 when clicked (toggle open and close)
+// DROP DOWN 1
+var dropDown1 = function() {
+  $("#drop-down-first").show("slow")
+}
+$(".click-me-static").click(dropDown1)
+// Show sub menu 1 when clicked (toggle open and close)
 $( "#drop-down1" ).click(function() {
   $( "#sub-menu1" ).toggle("slow", function(){
   })
 });
-
-// WORKS - Hides drop-down once selection's been made
+// Hides drop-down once selection's been made
 $(".wrap-menu").click(function(){
   $("#sub-menu1").hide("slow");
 });
-// WORKS - Hides the menu when clicking anywhere but inside the drop-down
+// Hides the menu when clicking anywhere but inside the drop-down
 $("#drop-down1").click(function(e){
   e.stopPropagation();
 });
 
 
 
-// WORKS - Show drop down 2 when clicked
+// DROP DOWN 2
 var dropDown2 = function() {
   $("#drop-down-second").show("slow")
 }
-$(".click-me1").click(dropDown2)
+$(".click-me-static2").click(dropDown2)
 // WORKS - Show sub menu 2 when clicked
 $( "#drop-down2" ).click(function() {
   $( "#sub-menu2" ).toggle("slow", function(){
   })
 });
-
 // WORKS - Hides drop-down once selection's been made
 $(".wrap-menu").click(function(){
   $("#sub-menu2").hide("slow");
@@ -67,11 +79,11 @@ $("#drop-down2").click(function(e){
 
 
 
-// Show sub menu 3
+// NEED TO CHANGE - Show sub menu 3
 var dropDown3 = function() {
   $("#drop-down-third").show("slow")
 }
-$(".click-me2").click(dropDown3)
+$(".click-me5").click(dropDown3)
 // WORKS - Show sub menu 3 when clicked
 $( "#drop-down3" ).click(function() {
   $( "#sub-menu3" ).toggle("slow", function(){
@@ -80,21 +92,21 @@ $( "#drop-down3" ).click(function() {
 
 
 
-// WORKS - Show sub menu 4
+// DROP DOWN 4
 var newRow = function() {
   $("#drop-down-fourth").show("slow")
 }
 $(".click-me1").click(newRow)
-// WORKS - Show sub menu 3 when clicked
+// Show sub menu 3 when clicked
 $( "#drop-down4" ).click(function() {
   $( "#sub-menu4" ).toggle("slow", function(){
   })
 });
-// WORKS - Hides drop-down once selection's been made
+// Hides drop-down once selection's been made
 $(".wrap-menu").click(function(){
   $("#sub-menu4").hide("slow");
 });
-// WORKS - Hides the menu when clicking anywhere but inside the drop-down
+// Hides the menu when clicking anywhere but inside the drop-down
 $("#drop-down4").click(function(e){
   e.stopPropagation();
 });
@@ -105,7 +117,7 @@ $("#drop-down4").click(function(e){
 var dropDown5 = function() {
   $("#drop-down-fifth").show("slow")
 }
-$(".click-me4").click(dropDown5)
+$(".click-me2").click(dropDown5)
 // WORKS - Show sub menu 2 when clicked
 $( "#drop-down5" ).click(function() {
   $( "#sub-menu5" ).toggle("slow", function(){
@@ -145,13 +157,21 @@ $("#uncheck-all").click(function() {
 $("#button1").click(function() {
   $("#row-1").clone().appendTo("#new-list")
 })
-
 // // DELETE BUTTON
-// var delete = function() {
+var deleteButton = function() {
+  $("#delete-button").show("slow")
+}
+$("#sub-menu3").click(deleteButton)
 
+// var deleteButton2 = function() {
+//   $("#delete-button2").show("slow")
 // }
+// $("#sub-menu5").click(deleteButton2)
 
-// $("delete-button").click(delete)
-
-
-
+var deleteRow = function() {
+  $("#drop-down-second").remove()
+  $("#drop-down-third").remove()
+  $("#drop-down-fifth").remove()
+  $("#delete-button").remove()
+}
+$("#delete-button").click(deleteRow)
