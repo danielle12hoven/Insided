e19a97519e5dd7984a27c595a218baf7df72ceec
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2017-02-26 22:29:20 -0500

    got a working append and delete for the bottom drop downs

--- public/js/script.js
+++ public/js/script.js
@@ -185,23 +185,19 @@ $(".click-me4").click(applyButton)
 var deleteButton = function() {
   $("#delete-button").show("slow")
 }
-// $("#apply-button").click(deleteButton)
-$(".click-me5").click(deleteButton)
-$(".click-me2").click(deleteButton)
-$(".click-me-static2").click(deleteButton)
-$("#check-all").click(deleteButton)
-
+$("#button1").click(deleteButton)
 
 
 var deleteRow = function() {
   $("#static-drop-down2").hide()
-  $("#drop-down-first").hide()
+  // $("#drop-down-first").hide()
   $("#drop-down-second").hide()
   $("#drop-down-third").hide()
-  $("#drop-down-fourth").hide()
+  // $("#drop-down-fourth").hide()
   $("#drop-down-fifth").hide()
   $("#delete-button").hide()
   $(".delete-tags").hide()
+  // $("#button1").hide()
 }
 $("#delete-button").click(deleteRow)
 
@@ -562,6 +558,7 @@ $(document).on("click", "#following-button", function(){
    $(this).remove();
 });
 
+
 var selectAllOptions = function(){
     $('#check-all').click(function(){
         var appendItem1 = $('<input type="button" id="profession-button" class="delete-tags" value="x profession"/>');
@@ -669,6 +666,68 @@ $('#check-highlight-10').click(function() {
     }
 });
 
+
+
+// APPEND DROPDOWNS TO BOTTOM OF SCREEN //
+var cloneInput1 = function(){
+    $('.click-me-static').click(function(){
+      if ($(this).is(':checked')) {
+        var appendInputStatic = $('#static-drop-down1').clone();
+        $(appendInputStatic).appendTo("#showInputs");
+      } else {
+        $("#showInputs").remove(appendInputStatic);
+      }
+});
+};
+cloneInput1()
+
+var cloneInput2 = function(){
+    $('.click-me-static2').click(function(){
+        var appendInputStatic2 = $('#static-drop-down2');
+        $(appendInputStatic2).clone().appendTo("#showInputs");
+});
+};
+cloneInput2()
+
+var cloneInput3 = function(){
+    $('.click-me1').click(function(){
+        var appendInput1 = $('drop-down-first');
+        $(appendInput1).clone().appendTo("#showInputs");
+});
+};
+cloneInput3()
+
+var cloneInput4 = function(){
+    $('.click-me2').click(function(){
+        var appendInput2 = $('drop-down-second');
+        $(appendInput2).clone().appendTo("#showInputs");
+});
+};
+cloneInput4()
+
+var cloneInput5 = function(){
+    $('.click-me3').click(function(){
+        var appendInput3 = $('drop-down-third');
+        $(appendInput3).clone().appendTo("#showInputs");
+});
+};
+cloneInput5()
+
+var cloneInput6 = function(){
+    $('.click-me4').click(function(){
+        var appendInput4 = $('drop-down-fourth');
+        $(appendInput4).clone().appendTo("#showInputs");
+});
+};
+cloneInput6()
+
+var cloneInput7 = function(){
+    $('.click-me5').click(function(){
+        var appendInput5 = $('drop-down-fifth');
+        $(appendInput5).clone().appendTo("#showInputs");
+});
+};
+cloneInput7()
 
 
 
